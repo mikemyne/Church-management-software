0651542132385c09515bac61006188a9c173b8ac
Mike Myne <myne2137@gmail.com>
2022-03-18 09:47:20 +0300

    initial commit

---
+++ pages/blog_4.php
@@ -0,0 +1,22 @@
+<?php require_once('../config.php'); ?>
+<!DOCTYPE HTML>
+<head>
+<title> blog 4 | <?php echo $_settings->info('short_name') ?></title>
+<meta name="description" content="">
+<meta name="keywords" content="sacrament">
+<meta name="robots" content="index, follow">
+<meta property="og:type" content="article" />
+<meta property="og:title" content="blog 4" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="http://localhost/church_management/uploads/blog_uploads/banners/5_banner.png" />
+<meta property="og:url" content="http://localhost/church_management/pages/blog_4.php" />
+<?php require_once('../inc/page_header.php') ?>
+</head>
+<?php include(base_app.'inc/body_block.php') ?>
+<h2>blog 4</h2>
+<input name="blog_id" value="e4da3b7fbbce2345d7772b0674a318d5" type="hidden">
+<hr>
+<small><span class='text-muted'><i class='fa fa-calendar-day'></i> Published: Jan 06,2022 09:14 AM</span></small>
+<center><img src="http://localhost/church_management/uploads/blog_uploads/banners/5_banner.png" class="img-thumbnail img-banner" alt="http://localhost/church_management/pages/blog_4.php" /></center>
+<p>Lorem ipsums</p>
+<?php include(base_app.'inc/body_block_end.php') ?>
